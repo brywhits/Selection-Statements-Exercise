b6a44f357dcb83505938744f6d1d6a7b0ba8df9e
Bryant Whitsett <bryantwhitsett71@gmail.com>
2023-05-16 00:31:06 -0400

    Creating the Selection Statement Exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Number Guessing Game
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+
+
+
+
         }
     }
 }
